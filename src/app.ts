609bbd4311ff806ed2713235cf1a6a16c5391d55
priscilaandreani <priscilaandreani@gmail.com>
2024-08-12 20:30:28 -0300

    chore: add error handling and logging to app.setErrorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
 import fastify from 'fastify';
 import { appRoutes } from './http/routes';
+import { ZodError } from 'zod';
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, req, reply) => {
+  if (error instanceof ZodError) {
+    reply
+      .status(400)
+      .send({ message: 'Validation error.', details: error.format() });
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.error(error);
+  } else {
+    // newlic.log(error);
+  }
+
+  reply.status(500).send({ message: 'Internal server error.' });
+});
