9ddefd3852fff808cc5422dd08e320e1e167081c
priscilaandreani <priscilaandreani@gmail.com>
2024-08-13 19:36:32 -0300

    feat: add check-in expiration validation
    This commit adds check-in expiration validation to the `ValidateCheckInService` class. If a check-in is validated more than 20 minutes after its creation, an `InvalidCheckInError` is thrown. This ensures that check-ins are validated within the specified time limit.

--- src/services/check-in/validate-check-in.test.ts
+++ src/services/check-in/validate-check-in.test.ts
@@ -1,8 +1,9 @@
-import { describe, expect, it, beforeEach } from 'vitest';
+import { describe, expect, it, beforeEach, vi, afterEach } from 'vitest';
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository';
 import { ValidateCheckInService } from './validate-check-in';
 import { CheckIn } from '@prisma/client';
 import { ResourceNotFoundError } from '../errors/resource-not-found-error';
+import { InvalidCheckInError } from '../errors/invalid-check-in-error';
 
 let checkInsRepository: InMemoryCheckInsRepository;
 let sut: ValidateCheckInService;
@@ -11,6 +12,12 @@ describe('checkIn service', () => {
   beforeEach(async () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckInService(checkInsRepository);
+
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
   });
 
   it('should be able to validate check in', async () => {
@@ -35,5 +42,23 @@ describe('checkIn service', () => {
         checkInId: 'non'
       })
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it('should not be able to validate the check-in ater 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2024, 0, 1, 13, 40)); // 01-01-2024 13:40
+    const TWENTY_ONE_MINUTES = 21 * 60 * 1000; //ms
+
+    const createdCheckIn: CheckIn = await checkInsRepository.create({
+      gym_id: 'gym-id',
+      user_id: 'user-id'
+    });
+
+    vi.advanceTimersByTime(TWENTY_ONE_MINUTES);
+
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id
+      })
+    ).rejects.toBeInstanceOf(InvalidCheckInError);
   });
 });
--- src/services/check-in/validate-check-in.ts
+++ src/services/check-in/validate-check-in.ts
@@ -1,6 +1,8 @@
 import { CheckInsRepository } from '@/repositories/check-ins-repository.types';
 import { CheckIn } from '@prisma/client';
 import { ResourceNotFoundError } from '../errors/resource-not-found-error';
+import dayjs from 'dayjs';
+import { InvalidCheckInError } from '../errors/invalid-check-in-error';
 
 interface ValidateCheckInServiceRequest {
   checkInId: string;
@@ -17,9 +19,19 @@ export class ValidateCheckInService {
     checkInId
   }: ValidateCheckInServiceRequest): Promise<ValidateCheckInServiceResponse> {
     const checkIn = await this.checkInsRepository.findById(checkInId);
+    const MAX_TIME_AFTER_CREATION = 20; // minutes
 
     if (!checkIn) {
       throw new ResourceNotFoundError();
+    }
+
+    const timeAfterCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes'
+    );
+
+    if (timeAfterCreation > MAX_TIME_AFTER_CREATION) {
+      throw new InvalidCheckInError('The checkin is expired');
     }
 
     checkIn.validated_at = new Date();
