02f72f47d28fe7e54c0b5a35181e806ddd510535
priscilaandreani <priscilaandreani@gmail.com>
2024-08-13 16:17:38 -0300

    feat: Add check-in validation for duplicate check-ins
    This commit adds validation to the `CheckInService` to prevent users from checking in multiple times on the same day. It introduces a new test case in the `checkIn service` test suite to verify this behavior.

--- src/services/check-in/check-in.test.ts
+++ src/services/check-in/check-in.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, beforeEach } from 'vitest';
+import { describe, expect, it, beforeEach, vi, afterEach } from 'vitest';
 import { CheckInService } from '@/services/check-in/check-in';
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository';
 
@@ -9,9 +9,49 @@ describe('checkIn service', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository();
     sut = new CheckInService(checkInsRepository);
+
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
   });
 
   it('should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-id',
+      userId: 'user-id'
+    });
+
+    expect(checkIn).toHaveProperty('id');
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it('should not be able to check in twice a day', async () => {
+    vi.setSystemTime(new Date(2024, 7, 13, 0, 0, 0));
+    await sut.execute({
+      gymId: 'gym-id',
+      userId: 'user-id'
+    });
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-id',
+        userId: 'user-id'
+      })
+    ).rejects.toBeInstanceOf(Error);
+  });
+
+  it('should be able to check in different day', async () => {
+    vi.setSystemTime(new Date(2024, 7, 13));
+
+    await sut.execute({
+      gymId: 'gym-id',
+      userId: 'user-id'
+    });
+
+    vi.setSystemTime(new Date(2024, 8, 13));
+
     const { checkIn } = await sut.execute({
       gymId: 'gym-id',
       userId: 'user-id'
