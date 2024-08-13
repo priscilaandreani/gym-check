ca277bec9d409a15c6559ca14d2bffef92d4f628
priscilaandreani <priscilaandreani@gmail.com>
2024-08-13 17:48:19 -0300

    feat: add check-in distance validation

--- src/services/check-in/check-in.test.ts
+++ src/services/check-in/check-in.test.ts
@@ -1,26 +1,45 @@
 import { describe, expect, it, beforeEach, vi, afterEach } from 'vitest';
 import { CheckInService } from '@/services/check-in/check-in';
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository';
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository';
+import { Decimal } from '@prisma/client/runtime/library';
+import { InvalidCheckInError } from '../errors/invalid-check-in-error';
 
 let checkInsRepository: InMemoryCheckInsRepository;
 let sut: CheckInService;
+let gymsRepository: InMemoryGymsRepository;
+let gymId: string;
 
 describe('checkIn service', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     checkInsRepository = new InMemoryCheckInsRepository();
-    sut = new CheckInService(checkInsRepository);
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new CheckInService(checkInsRepository, gymsRepository);
 
     vi.useFakeTimers();
+    const { id } = await gymsRepository.create({
+      name: 'Gym Name',
+      description: '',
+      latitude: new Decimal(userLatitude),
+      longitude: new Decimal(userLongitude)
+    });
+
+    gymId = id;
   });
 
   afterEach(() => {
     vi.useRealTimers();
   });
 
+  const userLatitude = -23.5668582;
+  const userLongitude = -46.660879;
+
   it('should be able to check in', async () => {
     const { checkIn } = await sut.execute({
-      gymId: 'gym-id',
-      userId: 'user-id'
+      gymId,
+      userId: 'user-id',
+      userLatitude,
+      userLongitude
     });
 
     expect(checkIn).toHaveProperty('id');
@@ -30,34 +49,61 @@ describe('checkIn service', () => {
   it('should not be able to check in twice a day', async () => {
     vi.setSystemTime(new Date(2024, 7, 13, 0, 0, 0));
     await sut.execute({
-      gymId: 'gym-id',
-      userId: 'user-id'
+      gymId,
+      userId: 'user-id',
+      userLatitude,
+      userLongitude
     });
 
     await expect(() =>
       sut.execute({
-        gymId: 'gym-id',
-        userId: 'user-id'
+        gymId,
+        userId: 'user-id',
+        userLatitude,
+        userLongitude
       })
-    ).rejects.toBeInstanceOf(Error);
+    ).rejects.toBeInstanceOf(InvalidCheckInError);
   });
 
   it('should be able to check in different day', async () => {
     vi.setSystemTime(new Date(2024, 7, 13));
 
     await sut.execute({
-      gymId: 'gym-id',
-      userId: 'user-id'
+      gymId,
+      userId: 'user-id',
+      userLatitude,
+      userLongitude
     });
 
     vi.setSystemTime(new Date(2024, 8, 13));
 
     const { checkIn } = await sut.execute({
-      gymId: 'gym-id',
-      userId: 'user-id'
+      gymId,
+      userId: 'user-id',
+      userLatitude,
+      userLongitude
     });
 
     expect(checkIn).toHaveProperty('id');
     expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.gyms.push({
+      id: 'gym-id_02',
+      name: 'Gym Name',
+      description: '',
+      latitude: new Decimal(-23.0),
+      longitude: new Decimal(-46.0)
+    });
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-id_02',
+        userId: 'user-id',
+        userLatitude,
+        userLongitude
+      })
+    ).rejects.toBeInstanceOf(InvalidCheckInError);
   });
 });
--- src/services/check-in/check-in.ts
+++ src/services/check-in/check-in.ts
@@ -1,9 +1,15 @@
 import { CheckInsRepository } from '@/repositories/check-ins-repository.types';
+import { GymsRepository } from '@/repositories/gyms-repository.types';
 import { CheckIn } from '@prisma/client';
+import { ResourceNotFoundError } from '../errors/resource-not-found-error';
+import { getDistanceBetweenCoordinates } from '../utils/getDistanceBetweenCoordinates';
+import { InvalidCheckInError } from '../errors/invalid-check-in-error';
 
 interface CheckInServiceRequest {
   userId: string;
   gymId: string;
+  userLatitude: number;
+  userLongitude: number;
 }
 
 interface CheckInServiceResponse {
@@ -11,19 +17,46 @@ interface CheckInServiceResponse {
 }
 
 export class CheckInService {
-  constructor(private checkInsRepository: CheckInsRepository) {}
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+    private gymsRepository: GymsRepository
+  ) {}
 
   async execute({
     gymId,
-    userId
+    userId,
+    userLatitude,
+    userLongitude
   }: CheckInServiceRequest): Promise<CheckInServiceResponse> {
+    const MAX_DISTANCE = 0.1; // 100 meters
+    const gym = await this.gymsRepository.findById(gymId);
+
+    if (!gym) {
+      throw new ResourceNotFoundError();
+    }
+
+    const distance = getDistanceBetweenCoordinates(
+      {
+        latitude: userLatitude,
+        longitude: userLongitude
+      },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber()
+      }
+    );
+
+    if (distance > MAX_DISTANCE) {
+      throw new InvalidCheckInError('Max distance reached.');
+    }
+
     const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
       userId,
       new Date()
     );
 
     if (checkInOnSameDate) {
-      throw new Error();
+      throw new InvalidCheckInError('User already checked in today.');
     }
 
     const checkIn = await this.checkInsRepository.create({
